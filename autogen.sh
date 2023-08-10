2a213314859ceaeac2d8987a4714f64e780f8825
Elijah Gordon <braindisassemblue@gmail.com>
2023-08-10 09:32:42 +0300

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -17,8 +17,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 if [ -e .gitmodules ] && [ -e .git ]; then
-    echo "* Updating git submodules"
-    git submodule update --init
+  echo "* Updating git submodules"
+  git submodule update --init
 fi
 
 : "${LIBTOOLIZE:=libtoolize}"
